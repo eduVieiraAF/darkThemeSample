d28ab21cb8aa374a8e867c4e3f2ddd9af0e89019
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-11-14 07:09:24 -0300

    all done

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -7,13 +7,20 @@ import { ETheme } from '../../enums/etheme.enums';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  public icon = ETheme.ICON_MOON
+  public icon: String = ETheme.ICON_MOON
+  public textTheme: String = ETheme.TEXT_DARK
 
   public toggleTheme() {
     const theme = document.body.classList.toggle('dark-theme');
 
-    if (theme) return this.icon = ETheme.ICON_SUN
-    else  return this.icon = ETheme.ICON_MOON;
+    if (theme) return (
+      this.icon = ETheme.ICON_SUN,
+      this.textTheme = ETheme.TEXT_LIGHT
+    )
 
+    else return (
+      this.icon = ETheme.ICON_MOON,
+      this.textTheme = ETheme.TEXT_DARK
+    )
   }
 }
